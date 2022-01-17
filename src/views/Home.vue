8cea4342feafa7599a5686ef72a6ebba0df466ad
Alperen Safak <a.safak@tr.emakina.com>
2022-01-17 11:59:03 +0300

    connected sanity with vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,74 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Welcome to Academy</h1>
+    <div class="courses">
+      <div class="loading" v-if="loading">Loading...</div>
+      <div v-if="error" class="error">
+        {{ error }}
+      </div>
+      <div class="container">
+        <div v-for="course in courses" class="course-item" :key="course._id">
+          <router-link :to="`/academy/${course.slug.current}`">
+            <h2>{{ course.title }}</h2>
+          </router-link>
+          <p>{{course.excerpt}}</p>
+          <hr />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import sanity from "../client";
+
+const query = `*[_type == "course"]{
+  _id,
+  title,
+  slug,
+  excerpt
+}[0...50]`;
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  data() {
+    return {
+      loading: true,
+      courses: [],
+    };
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      this.error = this.course = null;
+      this.loading = true;
+      sanity.fetch(query).then(
+        (courses) => {
+          this.loading = false;
+          this.courses = courses;
+        },
+        (error) => {
+          this.error = error;
+        }
+      );
+    },
+  },
+};
 </script>
+
+<style scoped>
+.home h1{
+    text-align: center;
+
+}
+.container {
+  margin: 0 auto;
+  max-width: 42em;
+  width: 100%;
+}
+.course-item {
+  box-sizing: border-box;
+}
+</style>
