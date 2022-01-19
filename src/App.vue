bdf838dd3f5bf0bf03bf112273694360a79113f1
Alperen Safak <a.safak@tr.emakina.com>
2022-01-19 11:45:34 +0300

    pure css codes to tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
+<section class="text-gray-600 body-font">
+  <div class="container px-5  mx-auto">
+ 
   <NavBar/>
   <router-view />
+  </div></section>
 </template>
 
 
@@ -15,78 +19,9 @@
   </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  display: flex;
-  flex-direction: column;
-  min-height: 100%;
-  min-height: 100vh;
-  padding: 1em 2em;
-  width: 100%;
-  margin-bottom: 8em;
-}
 
-#nav {
-  text-align: center;
 
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-}
-
-h1,
-h2,
-h3,
-h4,
-h5,
-h6 {
-  font-family: Avenir, sans-serif;
-  font-weight: 700;
-  line-height: 1.2;
-  margin: 0 0 0.5em 0;
-}
-
-h1 {
-  font-family: Roboto, serif;
-  font-size: 4em;
-  margin: 0 0 1em 0;
-}
-
-h2 {
-  margin: 1.6em 0 0 0;
-  font-size: 1.8em;
-}
-
-h3 {
-  font-size: 1.5em;
-}
-
-h4 {
-  font-size: 1.4em;
-}
-
-h5 {
-  font-size: 1.3em;
-}
-
-h6 {
-  font-size: 1.2em;
-}
 
 p,
 ul,
@@ -105,9 +40,7 @@ a:hover {
   color: #42b983;
 }
 
-img {
-  max-width: 100%;
-}
+
 
 hr {
   background-image: linear-gradient(
@@ -120,14 +53,7 @@ rgba(66, 185, 131, 1),    rgba(0, 0, 0, 0)
   margin: 40px auto;
 }
 
-blockquote {
-  border-left: 4px solid #cccccc;
-  font-size: 1.4em;
-  font-style: italic;
-  margin: 2rem 0;
-  padding-left: 2rem;
-  padding-right: 2rem;
-}
+
 
 .content h1 {
   font-size: 3em;
@@ -143,6 +69,7 @@ blockquote {
     font-size: 2.4em;
   }
 }
+
 
 @media (max-width: 480px) {
   body {
