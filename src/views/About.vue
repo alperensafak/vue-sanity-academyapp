3abac1f4ff7aca40c66f83435f18bcda7c2ff144
Alperen Safak <a.safak@tr.emakina.com>
2022-01-19 11:48:58 +0300

    About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1 class="text-center text-xl mt-4">This is an about page</h1>
+    <h1 class="text-center text-xl mt-4">This website includes Emakina Talent Academy Courses. You can track courses daily.</h1>
   </div>
 </template>
